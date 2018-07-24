7865bb44456d1fa876a8208824d35aa11d6896b4
Shepherd <shephedwang@163.com>
2018-07-24 14:54:05 +0800

    second commit

---
+++ src/main/java/com/hp/test/demo/DemoTestApplication.java
@@ -0,0 +1,25 @@
+package com.hp.test.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootConfiguration
+@SpringBootApplication
+public class DemoTestApplication {
+
+	public static void main(String[] args) {
+//		Long a = 1L;
+//		Long b = new Long(1L);
+//		Long c = new Long(1L);
+//		System.out.println(a==b);
+//		System.out.println(c==b);
+//		System.out.println(a==1L);
+//		System.out.println(b==1L);
+//		System.out.println(a.equals(b));
+//		System.out.println(a.equals(1L));
+//		System.out.println(a.equals(1));
+//		System.out.println(b.equals(1));
+		SpringApplication.run(DemoTestApplication.class, args);
+	}
+}
